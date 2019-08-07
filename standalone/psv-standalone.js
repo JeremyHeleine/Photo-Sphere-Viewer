7eaea68ebe9a4861a4f8f42e3afdf050fa3d3312
David Hein <44842054+MasterZydra@users.noreply.github.com>
2019-08-07 07:58:59 +0200

    Add necessary files for standalone use

---
+++ standalone/psv-standalone.js
@@ -0,0 +1,84 @@
+function anError(error) {
+    var errorMsg = document.createElement('div');
+    errorMsg.innerHTML = error;
+    document.getElementById('container').appendChild(errorMsg);
+}
+
+var viewer;
+function parseURLParameters() {
+    var URL;
+    if (window.location.hash.length > 0) {
+        // Prefered method since parameters aren't sent to server
+        URL = [window.location.hash.slice(1)];
+    } else {
+        URL = decodeURI(window.location.href).split('?');
+        URL.shift();
+    }
+    if (URL.length < 1) {
+        // Display error if no configuration parameters are specified
+        anError('No configuration options were specified.');
+        return;
+    }
+    URL = URL[0].split('&');
+    var configFromURL = {};
+
+    configFromURL.container = document.getElementById('container');
+    configFromURL.navbar = true;
+
+    for (var i = 0; i < URL.length; i++) {
+        var option = URL[i].split('=')[0];
+        var value = URL[i].split('=')[1];
+        if (value == '')
+            continue; // Skip options with empty values in URL config
+
+        // case distinction at time_anim, because boolean or number can be passed
+        if (option == 'time_anim') {
+            if (value == 'false') {
+                configFromURL[option] = JSON.parse(value);
+            } else {
+                configFromURL[option] = Number(value);
+            }
+            continue;
+        }
+
+        switch(option) {
+            case 'panorama': case 'anim_speed':
+                configFromURL[option] = decodeURIComponent(value);
+                break;
+            case 'size': case 'navbar':
+                configFromURL[option] = JSON.parse(decodeURIComponent(value));
+                break;
+            default:
+                anError('An invalid configuration parameter was specified: ' + option);
+                return;
+        }
+
+                    /*
+            case 'hfov': case 'pitch': case 'yaw': case 'haov': case 'vaov':
+            case 'minHfov': case 'maxHfov': case 'minPitch': case 'maxPitch':
+            case 'minYaw': case 'maxYaw': case 'vOffset': case 'autoRotate':
+            case 'autoRotateOnInactivity':
+                configFromURL[option] = Number(value);
+                break;
+            case 'autoLoad': case 'ignoreGPanoXMP': case 'hotSpotDebug':
+            case 'compass':
+                configFromURL[option] = JSON.parse(value);
+                break;
+            case 'author': case 'title': case 'firstScene': case 'fallback':
+            case 'preview': case 'panorama': case 'config':
+                configFromURL[option] = decodeURIComponent(value);
+                break;*/
+    }
+
+    // Create viewer
+    viewer = new PhotoSphereViewer(configFromURL);
+}
+
+// Display error if opened from local file
+if (window.location.protocol == 'file:') {
+    anError('Due to browser security restrictions, Pannellum can\'t be run ' +
+        'from the local filesystem; some sort of web server must be used.');
+} else {
+    // Initialize viewer
+    parseURLParameters();
+}
